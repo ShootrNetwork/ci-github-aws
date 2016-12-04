545407cfc2de7133382441197feb9fc4a827b51d
arturozv <zzarturozz@gmail.com>
2016-12-04 00:22:52 -0800

    simplify command exec (only 1), exit on error

--- command_exec.go
+++ command_exec.go
@@ -8,22 +8,17 @@ import (
 
 func exe_cmd_wait(command string) {
 	wg := new(sync.WaitGroup)
-	commands := []string{command}
-	for _, str := range commands {
-		wg.Add(1)
-		go exe_cmd(str, wg)
-	}
+	wg.Add(1)
+	go exe_cmd(command, wg)
 	wg.Wait()
 }
 
 func exe_cmd(cmd string, wg *sync.WaitGroup) {
 	log.Printf("Command: %s", cmd)
-	//parts := strings.Fields(cmd)
-	//out, err := exec.Command(parts[0], parts[1]).Output()
 	out, err := exec.Command("/bin/sh", "-c", cmd).CombinedOutput()
-	if err != nil {
-		log.Printf("Error occured: %s", err)
-	}
 	log.Printf("Output: %s", out)
+	if err != nil {
+		log.Fatalf("Error occured: %s", err)
+	}
 	wg.Done()
 }
