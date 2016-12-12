d1747b12af6cde3d75c32160f30f976c3c2f340d
arturozv <zzarturozz@gmail.com>
2016-12-12 09:37:49 -0800

    docker tag as bool and value as string

--- structs.go
+++ structs.go
@@ -9,20 +9,22 @@ type Params struct {
 type Config struct {
 	Region string `yaml:"region"`
 
-	Default_deploy         bool   `yaml:"default_deploy"`
-	Default_test_and_build bool   `yaml:"default_test_and_build"`
-	Default_build_docker   bool   `yaml:"default_build_docker"`
-	Default_docker_tag     string `yaml:"default_docker_tag"`
-	Default_upload_to_s3   bool   `yaml:"default_upload_to_s3"`
+	Default_deploy           bool   `yaml:"default_deploy"`
+	Default_test_and_build   bool   `yaml:"default_test_and_build"`
+	Default_build_docker     bool   `yaml:"default_build_docker"`
+	Default_docker_tag       bool   `yaml:"default_docker_tag"`
+	Default_docker_tag_value string `yaml:"default_docker_tag_value"`
+	Default_upload_to_s3     bool   `yaml:"default_upload_to_s3"`
 
 	BranchConfig []struct {
-		Branch       string `yaml:"branch"`
-		ASG          string `yaml:"asg"`
-		Deploy       bool   `yaml:"deploy"`
-		TestAndBuild bool   `yaml:"test_and_build"`
-		BuildDocker  bool   `yaml:"build_docker"`
-		DockerTag    string `yaml:"docker_tag"`
-		UploadToS3   bool   `yaml:"upload_to_s3"`
+		Branch         string `yaml:"branch"`
+		ASG            string `yaml:"asg"`
+		Deploy         bool   `yaml:"deploy"`
+		TestAndBuild   bool   `yaml:"test_and_build"`
+		BuildDocker    bool   `yaml:"build_docker"`
+		DockerTag      bool   `yaml:"docker_tag"`
+		DockerTagValue string `yaml:"docker_tag_value"`
+		UploadToS3     bool   `yaml:"upload_to_s3"`
 	} `yaml:"branch_config"`
 }
 
