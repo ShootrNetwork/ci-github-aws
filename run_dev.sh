856e6ca64d9ad177c9a1eda1661d53a886375f05
arturozv <zzarturozz@gmail.com>
2016-12-16 12:31:57 -0800

    rundev improvements

--- run_dev.sh
+++ run_dev.sh
@@ -1,17 +1,22 @@
 #docker kill java8-ci
 #docker rm java8-ci
-
-docker run -v /Users/arturo/git/shootr-backend:/ci -d --name java8-ci -t fav24/java8-ci bash
+#docker run -v /Users/arturo/git/shootr-backend:/ci -d --name java8-ci -t fav24/java8-ci bash
+docker run -v /Users/arturo/git/shootr-backend:/ci -d -t fav24/java8-ci bash
 
 # aws credentials
 #export AWS_ACCESS_KEY_ID=
 #export AWS_SECRET_ACCESS_KEY=
 source env_vars.sh
 
-go install && \
-ci-github-aws \
-  --c upload_to_s3 \
- --git-commit a1234b567 \
- --git-branch 'dev-'
+go install
 
+cd /Users/arturo/git/shootr-backend
+
+#ci-github-aws --c test_and_build  --git-commit a1234b567 --git-branch 'dev-'
+#ci-github-aws --c upload_to_s3    --git-commit a1234b567 --git-branch 'dev-'
+#ci-github-aws --c docker_build    --git-commit a1234b567 --git-branch 'dev-'
+ci-github-aws --c docker_tag      --git-commit a1234b567 --git-branch 'dev-'
+ci-github-aws --c deploy          --git-commit a1234b567 --git-branch 'dev-'
+
+cd /Users/arturo/git/go/src/github.com/shootrnetwork/ci-github-aws
 # --c test_and_build \
