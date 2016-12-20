c726448db8e48166a7482a8f354b282632b7a6da
arturozv <zzarturozz@gmail.com>
2016-12-20 10:45:45 -0800

    full build and test command

--- command_test_and_build.go
+++ command_test_and_build.go
@@ -13,9 +13,9 @@ func testAndBuild(params Params) {
 		start := time.Now()
 		log.Println("Test and build start...")
 
-		//command := fmt.Sprintf("docker exec java8-ci mvn --quiet --batch-mode -f %s/pom.xml clean test install jacoco:report coveralls:report", params.Config.PathInDocker)
+		command := fmt.Sprintf("docker exec java8-ci mvn --quiet --batch-mode -f %s/pom.xml clean test install jacoco:report coveralls:report", params.Config.PathInDocker)
 		//command := fmt.Sprintf("docker exec java8-ci mvn --quiet --batch-mode -f %s/pom.xml clean", params.Config.PathInDocker)
-		command := fmt.Sprintf("docker exec java8-ci mvn -f %s/pom.xml clean test install", params.Config.PathInDocker)
+		//command := fmt.Sprintf("docker exec java8-ci mvn -f %s/pom.xml clean test install", params.Config.PathInDocker)
 		exe_cmd_wait(command, 15*time.Minute)
 
 		log.Printf("Test and build done in %s", time.Since(start))
